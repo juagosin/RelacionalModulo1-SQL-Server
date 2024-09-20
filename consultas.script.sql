eb761e8829b8ffcdcb85d76e8bf83b94bd720be2
juagosin <juagosin@gmail.com>
2024-09-20 20:36:29 +0200

    Finalización de los ejercicios

--- consultas.script.sql
+++ consultas.script.sql
@@ -103,3 +103,60 @@ SELECT [CustomerId]
       ,[UnitPrice]
   FROM [LemonMusic].[dbo].[Track]
   where Composer = 'Queen & David Bowie'
+
+  --Listar las pistas de la playlist 'Heavy Metal Classic'
+  /*Vemos que la play list indicada es la número 17*/
+   Select Track.TrackId,Name, Composer from Track inner join
+  PlaylistTrack on PlaylistTrack.TrackId = Track.TrackId where PlaylistId = 17
+
+  --Listar las playlist junto con el número de pistas que contienen
+SELECT P.[PlaylistId]
+      ,P.[Name],
+	  count(PL.TrackId) as TrackNumber
+  FROM [LemonMusic].[dbo].[Playlist] as P
+
+  inner join   [LemonMusic].[dbo].[PlaylistTrack] as PL
+  on P.PlaylistId = PL.PlaylistId
+
+  group by   P.[PlaylistId],P.[Name]
+  --Listar las playlist (sin repetir ninguna) que tienen alguna canción de AC/DC
+SELECT 
+      Distinct(P.[Name])
+	  
+  FROM [LemonMusic].[dbo].[Playlist] as P
+
+  inner join   [LemonMusic].[dbo].[PlaylistTrack] as PL
+  on P.PlaylistId = PL.PlaylistId
+  inner join  [LemonMusic].[dbo].[Track] as T
+  on PL.TrackId = T.TrackId
+  where T.Composer = 'AC/DC'
+  group by  P.[Name]
+  --Listar las playlist que tienen alguna canción del artista Queen, junto con la cantidad que tienen
+SELECT 
+      Distinct(P.[Name]),
+	  Count(T.TrackId) as Cuantas
+	  
+  FROM [LemonMusic].[dbo].[Playlist] as P
+
+  inner join   [LemonMusic].[dbo].[PlaylistTrack] as PL
+  on P.PlaylistId = PL.PlaylistId
+  inner join  [LemonMusic].[dbo].[Track] as T
+  on PL.TrackId = T.TrackId
+  where T.Composer like '%Queen%'
+  group by  P.[Name]
+  --Listar las pistas que no están en ninguna playlist
+ SELECT T.*
+FROM [LemonMusic].[dbo].[Track] as T
+LEFT JOIN [LemonMusic].[dbo].[PlaylistTrack] as PL ON T.TrackId = PL.TrackId
+WHERE PL.TrackId IS NULL;
+  --Listar los artistas que no tienen album
+	SELECT  A.*
+	FROM [LemonMusic].[dbo].Artist  as A
+	LEFT JOIN [LemonMusic].[dbo].Album as AL ON A.ArtistId = AL.ArtistId
+WHERE AL.ArtistId IS NULL;
+  /*Listar los artistas con el número de albums que tienen
+Para saber si está bien, asegurate que algunos de los artistas de la query anterior (artistas sin album) aparecen en este listado con 0 albums*/
+	SELECT  A.Name, Count(AL.AlbumId)
+	FROM [LemonMusic].[dbo].Artist  as A
+	LEFT JOIN [LemonMusic].[dbo].Album as AL ON A.ArtistId = AL.ArtistId
+	GROUP BY A.Name
